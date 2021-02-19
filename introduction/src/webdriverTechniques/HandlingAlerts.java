141d51bfa6e780d7a3b95d64fcb55e57c4161798
gayathrie <pk.gayathri11@gmail.com>
2021-02-19 11:16:07 +0530

    commit

--- introduction/src/webdriverTechniques/HandlingAlerts.java
+++ introduction/src/webdriverTechniques/HandlingAlerts.java
@@ -1,9 +1,30 @@
 package webdriverTechniques;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class HandlingAlerts {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		// TODO Auto-generated method stub
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gayathrie\\Downloads\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys("gayathri");
+		
+		driver.findElement(By.id("alertbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().accept();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.findElement(By.id("name")).sendKeys("gayathri");
+		
+		driver.findElement(By.id("confirmbtn")).click();
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
 
 	}
 
