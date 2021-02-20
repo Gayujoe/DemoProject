fbf343d4f780548bb9ac2c20e2b937bff53c6aa4
gayathrie <pk.gayathri11@gmail.com>
2021-02-20 12:35:24 +0530

    Commit

--- introduction/src/webdriverTechniques/Actionsclass.java
+++ introduction/src/webdriverTechniques/Actionsclass.java
@@ -1,7 +1,9 @@
 package webdriverTechniques;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -9,14 +11,19 @@ public class Actionsclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gayathrie\\Downloads\\chromedriver.exe");
-		WebDriver driver=new ChromeDriver();
-		
-		driver.get("https://www.amazon.com/");
-		Actions a= new Actions(driver);
-		a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
 
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Gayathrie\\Downloads\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.amazon.com/");
+		driver.manage().window().maximize();
+		Actions a = new Actions(driver);
+		// Move to specific Element 
+		WebElement move = driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
+		a.moveToElement(driver.findElement(By.xpath("//a[@id='nav-link-accountList']"))).build().perform();
+		a.moveToElement(driver.findElement(By.id("twotabsearchtextbox"))).click().keyDown(Keys.SHIFT).sendKeys("hello").doubleClick().build().perform();
+		a.moveToElement(move).contextClick().build().perform();
+	
 	}
 
 }
